586525a87abaad5ef487b3f5057c0f5b94a1f70c
Wang Peiyu <oliviapy@udel.edu>
2017-11-30 22:23:27 -0500

    database set up done, Wang Peiyu's photo added

--- base.py
+++ base.py
@@ -1,6 +1,32 @@
-from flask import Flask, session, render_template, request, flash, redirect, url_for
-app = Flask(__name__)
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+# define database tables
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    director = db.Column(db.String(64))
+    actors = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
+
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    movies = db.relationship('Movie', backref='genre',cascade="delete")##back reference at the many side, unique in ORM
 
 @app.route('/')
 def index():
@@ -14,6 +40,15 @@ def show_all_members():
     # return "<h2>this is the page for all members</h2>"
     return render_template('member-all.html')
 
+@app.route('/genres')
+def show_all_genres():
+    genres = Genre.query.all()
+    return render_template('genre-all.html', genres=genres)
+
+@app.route('/movies')
+def show_all_movies():
+    movies = Movie.query.all()
+    return render_template('movie-all.html', movies=movies)
 
 # https://goo.gl/Pc39w8 explains the following line
 if __name__ == '__main__':
--- manage.py
+++ manage.py
@@ -1,12 +1,26 @@
 from flask_script import Manager
-from base import app
+from base import app, db, Movie, Genre
 
 manager = Manager(app)
 
 
 @manager.command
-def hello():
-    print "hello"
+def deploy():
+    db.drop_all()
+    db.create_all()
+    horror= Genre(name="Horror", description="Horror Films are unsettling films designed to frighten and panic, cause dread and alarm, and to invoke our hidden worst fears, often in a terrifying, shocking finale, while captivating and entertaining us at the same time in a cathartic experience.")
+    action=Genre(name="Action", description="Action film is a genre in which the protagonist or protagonists end up in a series of challenges that typically include violence, extended fighting, physical feats, and frantic chases.")
+    romantic=Genre(name="Romantic", description="Romance films or romance movies are romantic love stories recorded in visual media for broadcast in theaters and on TV that focus on passion, emotion, and the affectionate romantic involvement of the main characters and the journey that their genuinely strong, true and pure romantic love takes them through dating, courtship or marriage.")
+    split=Movie(name="Split", director="M. Night Shyamalan", actors="James McAvoy, Anya Taylor-Joy, Betty Buckley, Haley Lu Richardson", description="Split is a 2016 American psychological horror-thriller film written and directed by M. Night Shyamalan and starring James McAvoy, Anya Taylor-Joy, and Betty Buckley. The film follows a man with 23 different personalities who kidnaps and imprisons three teenage girls in an isolated underground facility.", genre=horror)
+    darkKnight=Movie(name="The Dark Knight", director=" Christopher Nolan", actors=" Christian Bale, Heath Ledger, Aaron Eckhart", description="When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.", genre=action)
+    titanic=Movie(name="Titanic", director="James Cameron", actors=" Leonardo DiCaprio, Kate Winslet, Billy Zane", description="A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", genre=romantic)
+    db.session.add(horror)
+    db.session.add(action)
+    db.session.add(romantic)
+    db.session.add(split)
+    db.session.add(darkKnight)
+    db.session.add(titanic)
+    db.session.commit()
 
 
 if __name__ == "__main__":
